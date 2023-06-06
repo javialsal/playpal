3e6727246cc12498c5dbfa72bb0c4f7749673748
Youval Akouka <youvalakouka@gmail.com>
2023-06-06 23:16:08 +0200

    Next games calculation

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,19 +1,10 @@
 class DashboardsController < ApplicationController
   def home
-    # array = []
-    # @next_games = current_user.participations.games.each do |game|
-    #   if game.start_at > Date.now
-    #     array << game
-    #   end
-
-    @user_games = Game.all.select { |game| game.participation.user == current_user }
-    @next_games = 
+    @next_games = []
+    current_user.participations.each do |participation|
+      if participation.game.start_at > DateTime.now
+        @next_games << participation.game
+      end
     end
-
-
-    # @monuments = current_user.monuments
-    # @demands = Booking.all.select { |booking| booking.monument.user == current_user }
-    # @future_bookings = @bookings.where(end_date: Date.today) + @bookings.where('start_date >= ?', Date.today)
-    # @passed_bookings = @bookings.where('end_date < ?', Date.today)
   end
 end
