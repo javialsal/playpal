59a2c98a8a568b7411faef29b96e695ec8f53731
Arnaud Lejade <arnaud.lejade1@gmail.com>
2023-06-06 12:29:36 +0200

    Added validates to models

--- app/models/game_type.rb
+++ app/models/game_type.rb
@@ -1,4 +1,6 @@
 class GameType < ApplicationRecord
   has_many :users
   has_many :games
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
