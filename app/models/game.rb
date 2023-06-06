53b5153b80155e228747724000015312778e0f9d
Arnaud Lejade <arnaud.lejade1@gmail.com>
2023-06-06 12:33:16 +0200

    Changed duration - added positivity

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,7 @@ class Game < ApplicationRecord
 
   validates :location, presence: true
   validates :start_at, presence: true
-  validates :duration, presence: true, numericality: { only_integer: true }
+  validates :duration, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
   validates :number_of_players, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 0, less_than_or_equal_to: 5 }
   validates :competitive, presence: true, inclusion: [true, false], exclusion: [nil]
 end
