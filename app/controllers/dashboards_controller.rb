c9df4fe91295b6c2aade17154416e4ee7aec141d
Youval Akouka <youvalakouka@gmail.com>
2023-06-06 21:32:24 +0200

    No push

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,14 @@
 class DashboardsController < ApplicationController
   def home
-    @games = current_user.participations.games
+    # array = []
+    # @next_games = current_user.participations.games.each do |game|
+    #   if game.start_at > Date.now
+    #     array << game
+    #   end
+
+    @user_games = Game.all.select { |game| game.participation.user == current_user }
+    @next_games = 
+    end
 
 
     # @monuments = current_user.monuments
