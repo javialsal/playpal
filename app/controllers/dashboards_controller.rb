b273b019b1ea506e32fee879b73d435043d35c53
Maxime Moreira <maxime.moreira@gmail.com>
2023-06-09 11:23:06 +0200

    added an order method on dashboard/index

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,9 @@
 class DashboardsController < ApplicationController
   def home
-    @next_games = []
-    current_user.participations.each do |participation|
-      @next_games << participation.game if participation.game.start_at > DateTime.now
-    end
+    @next_games = current_user.mygames.where("DATE(start_at) > ?", Date.today).order("start_at")
   end
 
   def mygames
-    @mygames = current_user.mygames
+    @mygames = current_user.mygames.order("start_at")
   end
 end
