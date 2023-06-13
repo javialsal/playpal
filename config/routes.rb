6902fb0502b1f2de15252d1cbcee3c0c02164ec6
Maxime Moreira <maxime.moreira@gmail.com>
2023-06-13 12:37:11 +0200

    added a new controller and a new chatroom show. working now on chat front

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'chatrooms/show'
   devise_for :users
   root to: "dashboards#home"
 
   resources :games do
     resources :participations, only: ['create']
+  end
+
+  resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
 
