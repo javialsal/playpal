13667aa85e7520fc5e9c34ab80fe61995e98a84a
Youval Akouka <youvalakouka@gmail.com>
2023-06-15 13:26:15 +0200

    Overall personality rating calculated on user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     # @victories = @user.mygames.select { |game| game.winner == @user }
     @victories = @user.victories_number
     @number_of_games = @user.past_games_with_score.count
+    @reviews = @user.participations.map(&:reviews)
+    personality = @reviews.map { |review| review.pluck(:personality_rating) }
+    personality_total = personality.map { |p| p.sum unless p.empty? }
+    @personality_rating = personality_total.compact.sum
   end
 
   private
